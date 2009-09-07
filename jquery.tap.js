e0fec10353004850c3b44a624e832f190b2855a5
Jiayong Ou <jiayong.ou@ymc.ch>
2009-09-07 16:49:57 +0200

    license notice

--- jquery.tap.js
+++ jquery.tap.js
@@ -1,3 +1,9 @@
+/*!
+ * ©2009 Jiayong Ou
+ * 
+ * License (MIT):
+ * http://github.com/jou/jquery.tap.js/blob/master/LICENSE
+ */
 (function(){
     jQuery.fn.tap = function(fn /* [callback_arg], [callback_arg], [...] */) {
         var args = jQuery.makeArray(arguments);
